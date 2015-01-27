989ff717d1bd06eb64413309b7e28d6831537859
tetoios <tetoioglou@glaroam2-152-215.wireless.gla.ac.uk>
2015-01-27 15:06:57 +0000

    Chapter 004 - completed

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 def index(request):
     #context_dict = {'boldmessage': "I am bold font from the context"}
     #return render(request, 'rango/index.html', context_dict)
+    #dasds
     return HttpResponse("Rango says hey there world!")
 
 def about(request):
