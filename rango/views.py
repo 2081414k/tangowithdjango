c8e156e5acf05abc2a7920e0f237d97d20b90db4
tetoios <tetoioglou@glaroam2-152-215.wireless.gla.ac.uk>
2015-01-27 14:57:29 +0000

    Chapter 004 - completed

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    return render(request, 'rango/index.html', context_dict)
+    #context_dict = {'boldmessage': "I am bold font from the context"}
+    #return render(request, 'rango/index.html', context_dict)
+    return HttpResponse("Rango says hey there world!")
 
 def about(request):
+    #context_dict2 = {'boldmessage': "Foivos Karounos"}
+    #return render(request, 'rango/about.html', context_dict2)
     return HttpResponse("Foivos Karounos")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +12,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), 
+    url(r'^rango/', include('rango.urls', namespace='index', app_name='django')),
+    url(r'^about/', include('rango.urls', namespace='about', app_name='django')),
 )
