c09073bff1fdab6d1fce41d2192c9b1f2cb69c96
tetoios <tetoioglou@glaroam2-165-87.wireless.gla.ac.uk>
2015-02-28 21:10:50 +0000

    Chapter 012 - completed  with '#' will be ignored, and an empty message aborts the commit.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,18 +3,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
 
 if not settings.DEBUG:
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls', namespace='index', app_name='django')),
     url(r'^about/', include('rango.urls', namespace='about', app_name='django')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls', namespace='accounts', app_name='django')),
 )
 
 if settings.DEBUG:
